6ed8f9e66703c7234430d96a04b425a7ccfa39ff
mateusmatosleonardo <m4teusmatos@gmail.com>
2025-07-29 19:53:24 -0300

    ui: update style header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
-'use client'
+"use client";
 
-import { useEffect, useState } from 'react';
-import Link from './Link';
-import { HiOutlineEnvelope } from 'react-icons/hi2';
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import Me from "@/assets/eu.png";
 
 export default function Header() {
   const [scroll, setScroll] = useState(0);
@@ -12,36 +12,32 @@ export default function Header() {
       setScroll(window.scrollY);
     };
 
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
-    <header className={`fixed w-full py-5 ${scroll > 0 ? 'bg-white shadow-md' : ''}`}>
-      <div className='flex justify-between items-center container mx-auto'>
-        <div className='flex items-center space-x-3'>
-          <HiOutlineEnvelope className='text-dark_primary' />
-          <p className='text-sm text-dark_primary'>
-            m4teusleonardo@gmail.com
-          </p>
+    <header
+      className={`fixed w-full py-3.5 z-10 ${scroll > 0 ? "bg-black/90 shadow-md" : ""}`}
+    >
+      <div className="flex justify-between items-center container mx-auto">
+        <div className="flex items-center space-x-3">
+          <Image
+            src={Me}
+            width={40}
+            height={40}
+            alt="profile-image"
+            quality={100}
+            className="rounded-full"
+          />
+          <p className="font-medium text-white">Mateus Leonardo</p>
         </div>
         <div>
-          <div className="flex space-x-6 text-[#F7F7F7]">
-            <Link>
-              Sobre
-            </Link>
-            <Link>
-              Experiência
-            </Link>
-            <Link>
-              Portfólio
-            </Link>
-            <Link>
-              Contato
-            </Link>
-          </div>
+          <button className="text-sm font-medium px-4 py-2.5 rounded-3xl bg-[#222121] text-white">
+            Entre em contato
+          </button>
         </div>
       </div>
     </header>
-  )
+  );
 }

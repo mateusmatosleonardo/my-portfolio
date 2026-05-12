e29f47b6b52138f7ad19befdc49d0c053b32e114
Mateus Leonardo <m4teusmatos@gmail.com>
2026-05-11 23:02:20 -0300

    style: adjust TechItem component dimensions and improve subtitle formatting

--- src/components/tech-item.tsx
+++ src/components/tech-item.tsx
@@ -10,19 +10,21 @@ interface TechItemProps {
 
 export default function TechItem(props: TechItemProps) {
   return (
-    <div className="flex w-full h-16 rounded-lg border border-dark-gray">
-      <div className="flex items-center justify-center w-16 h-16 border-r border-dark-gray">
+    <div className="flex w-full h-[5rem] rounded-lg border border-dark-gray">
+      <div className="flex items-center justify-center w-[5rem] h-[5rem] border-r border-dark-gray">
         <Image
           src={props.image}
-          width={26}
-          height={26}
+          width={30}
+          height={30}
           alt={props.alt}
           className={props.invert ? "invert" : ""}
         />
       </div>
       <div className="flex-1 flex flex-col justify-center px-4">
         <p className="text-[0.92rem]">{props.title}</p>
-        <p className="text-[0.80rem] text-default-gray">{props.subtitle}</p>
+        <p className="text-[0.80rem] leading-[1.2rem] text-default-gray">
+          {props.subtitle}
+        </p>
       </div>
     </div>
   );
